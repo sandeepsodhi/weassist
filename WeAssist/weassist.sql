ef1c6640eb0b4b50f5795d0c9562202c0554ab65
SW <SW>
2016-02-09 14:49:10 +0530

    updated database file

--- WeAssist/weassist.sql
+++ WeAssist/weassist.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2016 at 02:23 PM
+-- Generation Time: Feb 09, 2016 at 10:16 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -24,14 +24,23 @@ DELIMITER $$
 --
 -- Procedures
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agent`(IN `fname` VARCHAR(30), IN `lname` VARCHAR(30), IN `uname` VARCHAR(30), IN `pass` VARCHAR(30), IN `utype` VARCHAR(30), IN `rcode` VARCHAR(30), IN `ruser` VARCHAR(30))
+INSERT INTO users(f_name,l_name,u_name,pswd,u_type,r_code,r_user)  VALUES(fname,lname,uname,pass,utype,rcode,ruser)$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insdata`(IN `uname` VARCHAR(50), IN `fname` VARCHAR(50), IN `lname` VARCHAR(50), IN `pswdd` VARCHAR(50), IN `contactt` INT(50))
 INSERT INTO users(u_name,f_name,l_name,pswd,contact) VALUES (uname,fname,lname,pswdd,contactt)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `login`(IN `uname` VARCHAR(50), IN `pswdd` VARCHAR(50))
-select f_name from users where u_name = uname AND pswd = pswdd$$
+select u_name from users where u_name = uname AND pswd = pswdd$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ruser`(IN `u_namee` VARCHAR(50), IN `pswdd` VARCHAR(50))
+select r_code from users where u_name=u_namee AND pswd=pswdd$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `social_login`(IN `fname` VARCHAR(50), IN `lname` VARCHAR(50), IN `uname` VARCHAR(50))
 insert into users(f_name,l_name,u_name) values(fname,lname,uname)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `worker`(IN `fname` VARCHAR(30), IN `lname` VARCHAR(30), IN `uname` VARCHAR(30), IN `pass` VARCHAR(30), IN `contact` VARCHAR(30), IN `utype` VARCHAR(30), IN `image` VARCHAR(255), IN `rcode` VARCHAR(255), IN `ruser` VARCHAR(50))
+INSERT INTO users(f_name,l_name,u_name,pswd,contact,u_type,image_upload,r_code,r_user)VALUE(fname,lname,uname,pass,contact,utype,image,rcode,ruser)$$
 
 DELIMITER ;
 
@@ -87,28 +96,32 @@ CREATE TABLE IF NOT EXISTS `sub_categoty` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `u_id` int(10) NOT NULL,
+  `u_type` varchar(50) DEFAULT 'Customer',
+  `r_code` varchar(200) NOT NULL,
+  `r_user` varchar(40) NOT NULL,
   `u_name` varchar(30) NOT NULL,
   `pswd` varchar(50) NOT NULL,
   `profile_pic` varchar(255) NOT NULL DEFAULT 'image/NewCandidateImage.jpg',
   `f_name` varchar(50) NOT NULL,
   `l_name` varchar(50) NOT NULL,
   `contact` bigint(11) NOT NULL,
+  `image_upload` varchar(100) NOT NULL,
   `city` varchar(255) NOT NULL,
   `is_active` varchar(10) NOT NULL DEFAULT 'YES',
   `entry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `activation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`u_id`, `u_name`, `pswd`, `profile_pic`, `f_name`, `l_name`, `contact`, `city`, `is_active`, `entry_date`, `activation_date`) VALUES
-(2, 'sandeep@gmail.com', 'sodhi', 'image/NewCandidateImage.jpg', 'singh', 'sodhi', 21214124, '', 'YES', '2016-01-27 06:30:50', '2016-01-27 06:30:50'),
-(5, 'sodhi@mailinator.com', 'sodhi', 'image/NewCandidateImage.jpg', 'sodhi', 'sandeep', 325023, '', 'YES', '2016-01-27 09:40:49', '2016-01-27 09:40:49'),
-(14, 'infotechtraining1761@gmail.com', '', 'image/NewCandidateImage.jpg', 'Infotech', 'Training', 0, '', 'YES', '2016-01-27 14:19:03', '2016-01-27 14:19:03'),
-(18, 'vickysodhi15@gmail.com', '', 'image/NewCandidateImage.jpg', 'Sandeep', 'Sodhi', 0, '', 'YES', '2016-01-27 16:07:30', '2016-01-27 16:07:30'),
-(20, 'infotechtraining2016@gmail.com', '', 'image/NewCandidateImage.jpg', 'Infotech', 'Singh', 0, '', 'YES', '2016-01-28 04:35:44', '2016-01-28 04:35:44');
+INSERT INTO `users` (`u_id`, `u_type`, `r_code`, `r_user`, `u_name`, `pswd`, `profile_pic`, `f_name`, `l_name`, `contact`, `image_upload`, `city`, `is_active`, `entry_date`, `activation_date`) VALUES
+(1, 'agent', '8LZ8dug6', 'NULL', 'sumanjeet46@gmail.com', 'hudgyuh', 'image/NewCandidateImage.jpg', 'suman', 'jeet', 0, '', '', 'YES', '2016-02-09 05:35:15', '2016-02-09 05:35:15'),
+(2, 'customer', 'NULL', 'NULL', 'jj@hjfjfjjk', 'jatin', 'image/NewCandidateImage.jpg', 'jatin', 'roy', 0, '', '', 'YES', '2016-02-09 06:27:40', '2016-02-09 06:27:40'),
+(3, 'worker', 'NULL', 'ggg', 'sukhneet@yahoo.com', 'sukh', 'image/NewCandidateImage.jpg', 'sukj', 'sjksj', 0, '', '', 'YES', '2016-02-09 06:28:37', '2016-02-09 06:28:37'),
+(4, 'worker', 'NULL', 'NULL', 'kjshj@hjdxhj', 'hjcxhj', 'image/NewCandidateImage.jpg', 'sukh', 'hjksjkjhsw', 0, 'IMG-20151226-WA0011.jpg', '', 'YES', '2016-02-09 06:30:45', '2016-02-09 06:30:45'),
+(5, 'worker', 'NULL', '8LZ8dug6', 'edwffdwqfd@fsdddd', 'jhfj', 'image/NewCandidateImage.jpg', 'hdshjdj', 'hjsdjdcjh', 0, 'IMG-20151226-WA0011.jpg', '', 'YES', '2016-02-09 06:35:06', '2016-02-09 06:35:06');
 
 -- --------------------------------------------------------
 
@@ -168,7 +181,7 @@ ALTER TABLE `sub_categoty`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
