3b7a6079db99244bfb1844e796487441821d827c
SW <SW>
2016-04-01 10:29:47 +0530

    updated user header file

--- WeAssist/view/user/pages/header.php
+++ WeAssist/view/user/pages/header.php
@@ -172,73 +172,25 @@ if($_SESSION['u_type']=='Customer')
               <strong><center>Help & Support</center></strong>
             </div>
           </div>
-          
-          <!-- here chat window code -->
-
-
 </div>  
 <?php 
 }
-elseif($_SESSION['u_type']=='agent'){
+elseif($_SESSION['u_type']=='agent' || $_SESSION['u_type']=='worker'){
 ?>
 <!--class ="demo-chat-create" for conneting -->
-<div  onclick="onCreateChannel()" style="position:fixed;z-index:20;width:280px;bottom:0px;right:2px;padding:2px;scroll-behavior:auto;">
-          <div onclick="show()" style="border-radius:4px;background-color:#dd4b39;color:white;height:25px"><div><strong><center>Help & Support</center></strong></div></div>
-          <div style="display: none" id="hbar">
-              <div class="box box-success">
-                <div class="box-header">
-                  <i class="fa fa-comments-o"></i>
-                  <h3 class="box-title">Chat</h3>
-                </div>
-                <div class="box-body chat" id="chat-box">
-                  <!-- chat item -->
-                  <div class="item">
-                   <!-- WebRTC demo -->
-                      <div class="demo" id="demo" style="min-height: 100px;height: 200px; overflow-y:auto">
-                        <div class="demo-connect">
-                          <!-- <li class="list-group-item" data-remove="true"><br>&nbsp&nbspConnecting...</li> -->
-                          <?php
-                          include '../../../model/dbConnect.php';
-                              $ch = mysqli_fetch_row(mysqli_query($conn,"select channel from chat_user where pr_id in(select pr_id from profession where u_id='".$_SESSION['u_id']."')"));    
-                              echo "<input type='hidden' value='".$ch[0]."' class='demo-chat-channel-input'></input>";
-                            ?>
-                            
-                            <p><b>Connecting...<br></b></p>
-                              
-                            <!-- <button visibility='hidden' type='hidden' class="demo-chat-create btn btn-primary">Create</button> -->
-                            <input type='hidden' class="demo-chat-create btn btn-primary">
-              
 
-                            <!-- <input type='hidden' class="demo-chat-create btn btn-primary" value='Create'></input> -->
-                          <input type="hidden" style='margin-top:5px' class="demo-chat-join btn btn-warning col-xs-4 col-sm-4 pull-right">
-                        </div>
-                        <div class="demo-chat inactive">
-                          <ul class="demo-chat-messages list-group">
-                            <li class="list-group-item" data-remove="true">No chat messages available</li>
-                          </ul>
-                        </div>
-                      </div>
-                      <!-- /End WebRTC demo -->
-                  </div>  
-                  <!-- /.item -->
-                </div>
-                <!-- /.chat -->
+<div style="position:fixed;z-index:20;width:280px;bottom:0px;right:2px;padding:2px;scroll-behavior:auto;">
+          <!-- <div onclick="show()" style="border-radius:4px;background-color:#dd4b39;color:white;height:25px"> -->
+          <div onclick="window.open('chat.php','width=100px', 'height=370px')" style="border-radius:4px;background-color:#dd4b39;color:white;height:25px">
 
-                <div class="box-footer">
-                  <div class="demo-chat-input">
-                         <input name="message" style="width:80%" class="demo-chat-message-input form-control" onkeyup="javscript: if (event.keyCode==13) { sc_roll();/*$('.demo').scrollTop=$('.demo').scrollHeight;*/}"placeholder="Message" on></input>
-                         <button style="background-color:#dd4b39;margin-left:78%;margin-top:-58px;width:20%px " onclick="sc_roll();" class="demo-chat-send btn btn-primary">Send</button>
-                  </div>
-                </div>
-              </div>
-              <!-- /.box (chat box)  javascript:$('#demo').scrollTop=$('#demo').scrollHeight -->
-             <div id="sh" visibility="hidden"></div>
+            <div>
+              <strong><center>Help & Support</center></strong>
+            </div>
           </div>
-</div>
+</div>  
 <?php    
 }
 ?>
-
 
 <!-- Web rtc -->
 <!-- Zepto for AJAX -->
@@ -251,7 +203,7 @@ elseif($_SESSION['u_type']=='agent'){
 <script src="chat/js/datachannel-demo.js"></script>
 <!-- Our WebRTC application -->
 <script src="chat/js/datachannel-demo.js"></script>
-<!-- Fill channel name -->
+<!-- Fill channel name  -->
 
 <script type="text/javascript">
 $(document).ready(function(){
@@ -289,6 +241,7 @@ $("#subcat").html(html);
     document.querySelector(".demo-chat-channel-input").value = channel;
   }
 </script>
+
 <!-- Chat system -->
 <script type="text/javascript">
   function show(){
